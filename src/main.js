6605fca4611851b1b2a55656abbcaf912b561a21
Hanna Söderström <gothbarbie84@gmail.com>
2017-06-13 16:13:05 +0200

    Implemented GraphQL

--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import ApolloClient, {
-  createNetworkInterface,
-  IntrospectionFragmentMatcher
-} from 'apollo-client'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import VueApollo from 'vue-apollo'
 import store from './store'
 import router from './router'
+
+const apolloClient = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: `${__API_URL__}/graphql`,
+    transportBatching: true
+  }),
+  connectToDevTools: true,
+  dataIdFromObject: r => r.id
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  apolloProvider,
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
